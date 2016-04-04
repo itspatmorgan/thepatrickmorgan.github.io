a5265feaa89a45dfee8c2db3fb7fe6d7aea03ea9
patrickmmorgan <pamorgan12@gmail.com>
2016-04-04 12:57:11 -0400

    updating with full-bleed picture

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
   
+
+  // //PARALLAX -----------------
+
+  // $(window).scroll(function(){
+  //   var wScroll = $(this).scrollTop();
+
+  //   //Header --------------
+  //   $('.js-parallax-element').css({
+  //     'transform' : 'translate(0px, '+ wScroll /3 + '%)'
+  //   });
+
+  //   console.log("scrolling");
+  // });
+
+  console.log("loaded");
 });
