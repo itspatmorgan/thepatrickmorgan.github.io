3ca50e8b3f4b87629e68f29bdcf67ea5e43c4d02
patrickmmorgan <pamorgan12@gmail.com>
2016-04-13 19:22:41 -0400

    implemented simple slick go to slider on tech stack page

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,18 +1,43 @@
 $(document).ready(function(){
   
+  var elements = {
+    carousel: $('.js-carousel'),
+    selectors: $(".js-carousel-control")
+  };
 
-  // //PARALLAX -----------------
+  var go_to_settings = {
+    infinite: false,
+    slidesToShow: 1,
+    speed: 500,
+    arrows: false,
+    fade: true,
+    swipe: false,
+    adaptiveHeight: true
+  };
 
-  // $(window).scroll(function(){
-  //   var wScroll = $(this).scrollTop();
+  elements.carousel.slick(go_to_settings);
 
-  //   //Header --------------
-  //   $('.js-parallax-element').css({
-  //     'transform' : 'translate(0px, '+ wScroll /3 + '%)'
-  //   });
+  var updateActiveSelector = function ($clicked_selector) {
+    // Remove all active toggles
+    elements.selectors.removeClass('active');
+    // Update the clicked toggle to active
+    $clicked_selector.addClass("active");
+  };
 
-  //   console.log("scrolling");
-  // });
+  var setToggleEvents = function() {
+    // Loop through toggle elements in elements.toggles array
+    elements.selectors.each(function (index) {
+      // On click of a toggle,
+      $(this).on('click', function () {
+        // Update the active toggle
+        updateActiveSelector($(this));
+        // Go to the related index slide in the Slick Carousel
+        elements.carousel.slick('slickGoTo', index);
+      });
+    });
+  };
+
+  setToggleEvents();
 
   console.log("loaded");
 });
